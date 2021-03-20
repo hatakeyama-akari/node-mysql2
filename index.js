400f2a3cdc15fab17a06b07416c5af2bbcf9712a
hatakeyama-akari <hatakeyama@usedge.tech>
2021-03-20 11:29:37 +0900

    Promise wrappers with Async Await

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-const mysql = require('mysql2');
 const config = require('config');
 
-const connection = mysql.createConnection({
-  ...config.mysql,
-});
+main();
 
-connection.execute(
-  'SELECT * FROM `category_master` WHERE `category_id` = ? OR `category_name` = ?',
-  [2, 'leisure'],
-  function (err, results, fields) {
-    console.log(results); // results contains rows returned by server
-    // console.log(fields); // fields contains extra meta data about results, if available
+async function main() {
+  // get the client
+  const mysql = require('mysql2/promise');
+  // create the connection
+  const connection = await mysql.createConnection({
+    ...config.mysql,
+  });
 
-    // If you execute same statement again, it will be picked from a LRU cache
-    // which will save query preparation time and give better performance
-  }
-);
-
-connection.execute('select 1 + ? + ? as result', [5, 6], (err, rows) => {
+  // query database
+  const [
+    rows,
+    fields,
+  ] = await connection.query(
+    'SELECT * FROM `category_master` WHERE `category_id` = ? OR `category_name` = ?',
+    [2, 'leisure']
+  );
   console.log(rows);
-  // rows: [ { result: 12 } ]
-  // internally 'select 1 + ? + ? as result' is prepared first. On subsequent calls cached statement is re-used
-});
 
-connection.unprepare('select 1 + ? + ? as result');
+  connection.end();
 
-connection.end();
-
-console.log('kitayo');
+  console.log('kitayo');
+}
